5e996cbdd78120ab285f78b5a065194459cd23d4
Rohit <tomarrohit5034@gmail.com>
2022-11-30 15:16:22 +0000

    doubt

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Rohit',
+  role: 'Web Developer',
+}
+const App = () => <h1>Users List App</h1>
+
+export default App
